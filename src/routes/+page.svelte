b00aff113128698ad556c9e5e6d67930a20bce02
haiz <haiz.gigs@gmail.com>
2024-10-17 11:25:41 +0000

    t; added intro text

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,3 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
-<div class="flex items-center gap-4">
-	<button type="button" class="btn-icon preset-filled">&rarr;</button>
-	<button type="button" class="btn preset-filled">Button</button>
-	<button type="button" class="btn preset-filled">
-		<span>Button</span>
-		<span>&rarr;</span>
-	</button>
+<div class="space-y-4 flex flex-col place-items-center">
+  <p> Hia, i m haiz.</p>
 </div>
