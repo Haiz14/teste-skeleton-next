80e2cd9afea8182da5928821b90f7aea24d8e04b
Haiz14 <haiz.gigs@gmail.com>
2024-10-17 15:12:28 +0530

    init skeleton next

---
+++ src/routes/+page.svelte
@@ -0,0 +1,10 @@
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="flex items-center gap-4">
+	<button type="button" class="btn-icon preset-filled">&rarr;</button>
+	<button type="button" class="btn preset-filled">Button</button>
+	<button type="button" class="btn preset-filled">
+		<span>Button</span>
+		<span>&rarr;</span>
+	</button>
+</div>
